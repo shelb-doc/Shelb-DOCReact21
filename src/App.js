2cdb99c5304ce190dffc02aba81b1dbbe059fbba
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-01-29 10:57:04 -0500

    added nav and footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Contact from "./containers/Contact/Contact";
 import Home from "./containers/Home/Home";
 import Portfolio from "./containers/Portfolio/Portfolio";
-
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Switch>
-          <Route path="/" component={Home} />
           <Route path="/portfolio" component={Portfolio} />
           <Route path="/contact" component={Contact} />
+          <Route exact path="/" component={Home} />
         </Switch>
+        <Footer/>
       </Router>
     </div>
   );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+return (
+    <div>
+    <footer class="py-5 bg-gray-700 text-center text-white">
+        Tailwind is Awesome 😎
+    </footer>
+    </div>
+);
+};
+
+export default Footer;
