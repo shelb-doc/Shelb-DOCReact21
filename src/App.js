7864a1649a4c379a68ebefbb69cd9690581eed6d
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-01-29 10:05:54 -0500

    Fixed Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Contact from "./containers/Contact/Contact";
+import Home from "./containers/Home/Home";
+import Portfolio from "./containers/Portfolio/Portfolio";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO</h1>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
     </div>
   );
 }
