b5e89917c3f3ff253005f8cfc086ca77fece39ef
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-01-30 20:34:17 -0500

    added Portfilo

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+// import { Link } from "react-router-dom";
+// import Footer from "../../components/Footer";
+// import assets from "../../../public/assets/portfolio-images";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>This is my Home</h1>
+return (
+    <div>
+    <div className="container">
+        <div className="row">
+        <div className="col-sm">
+            <h4>Brand Statement</h4>
+            <h3>Brief History</h3>
+            Hello! My name is Shelby De Oliveira Cignetti. I am a student at
+            Georgia Tech Bootcamp for Coding. I have a Bachelor’s in Fine Arts.
+            I have a cat name Mimikyu and she was a stray. I was born in
+            Pittsburgh, Pennsylvania where I lived in Latrobe, Pennsylvania
+            until 2002. Then I moved to the Golden Isles in Southeast Georgia.
+            Then in 2010, I moved to Atlanta for college. In my free time, I
+            like to play videogames, hike, rock climbing, and volunteer at
+            Dragoncon and Momocon.
+            <h3>Professional Background</h3>
+            I am a Quality Assurance Engineer
+            with technical proficiency in HTML, CSS, Javascript, JQuery,
+            Bootstrap, testing of payment gateways and card terminals. Recently
+            gained notable graphic design experience from the Art Institute,
+            also acquired a Practitioner certification with Amazon Web Services
+            and will be receiving a Certificate in Coding from Georgia Tech
+            Professional Services. Recently worked in a project where I utilized
+            my passion for technology and excellent communication skills to
+            develop Postman test suites that validated the functionality of
+            legacy services. I am a team player with good communication skills
+            and a passion for learning new skills. Passionate about creating
+            life-enhancing user experiences and collaborating with others to
+            develop meaningful mobile and web applications. My educational and
+            professional background in technology, combined with a passionate
+            drive to effectively solve problems, would make me a strong addition
+            to any organization.
+            <ul>
+            Soft skills include verbal and written communication, enthusiasm,
+            positivity, and a competitive spirit to bring the best out of
+            myself and those with whom I work.
+            </ul>
         </div>
-    );
+        </div>
+    </div>
+    </div>
+);
 };
 
 export default Home;
--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -1,11 +1,118 @@
-import React from 'react';
+import React from "react";
+import "./Portfolio.css";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import projects from "../../projects.json";
+import trackImage from "../../assets/thumbnails/comicBookFinder.png";
+import seniorImage from "../../assets/portfolio-images/Senior-Travel-Covid-Care.gif";
+import quizImage from "../../assets/portfolio-images/CodingQuiz.png";
+import passwordImage from "../../assets/portfolio-images/Password-Generator.gif";
 
-const Portfolio = () => {
-    return (
-        <div>
-            <h1>This is my Portfolio</h1>
+function Portfolio() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-12">
+          <h1>Portfolio</h1>
         </div>
-    );
-};
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          <ProjectCard
+            id={projects[6].id}
+            key={projects[6].id}
+            name={projects[6].name}
+            image={projects[6].image}
+            alt={projects[6].alt}
+            description={projects[6].description}
+            link={projects[6].link}
+          />
+        </div>
+        <div className="col-sm-6">
+          <ProjectCard
+            id={projects[7].id}
+            key={projects[7].id}
+            name={projects[7].name}
+            image={projects[7].image}
+            alt={projects[7].alt}
+            description={projects[7].description}
+            link={projects[7].link}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          <ProjectCard
+            id={projects[0].id}
+            key={projects[0].id}
+            name={projects[0].name}
+            image={trackImage}
+            alt={projects[0].alt}
+            description={projects[0].description}
+            link={projects[0].link}
+          />
+        </div>
+        <div className="col-sm-6">
+          <ProjectCard
+            id={projects[1].id}
+            key={projects[1].id}
+            name={projects[1].name}
+            image={seniorImage}
+            alt={projects[1].alt}
+            description={projects[1].description}
+            link={projects[1].link}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          <ProjectCard
+            id={projects[2].id}
+            key={projects[2].id}
+            name={projects[2].name}
+            image={projects[2].image}
+            alt={projects[2].alt}
+            description={projects[2].description}
+            link={projects[2].link}
+          />
+        </div>
+        <div className="col-sm-6">
+          <ProjectCard
+            id={projects[3].id}
+            key={projects[3].id}
+            name={projects[3].name}
+            image={quizImage}
+            alt={projects[3].alt}
+            description={projects[3].description}
+            link={projects[3].link}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          <ProjectCard
+            id={projects[4].id}
+            key={projects[4].id}
+            name={projects[4].name}
+            image={passwordImage}
+            alt={projects[4].alt}
+            description={projects[4].description}
+            link={projects[4].link}
+          />
+        </div>
+        <div className="col-sm-6">
+          <ProjectCard
+            id={projects[5].id}
+            key={projects[5].id}
+            name={projects[5].name}
+            image={projects[5].image}
+            alt={projects[5].alt}
+            description={projects[5].description}
+            link={projects[5].link}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Portfolio;
