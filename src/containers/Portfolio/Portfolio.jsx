21e40c01a8f95c86d91bc3b38f774c704bf56c5b
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-01-30 21:43:17 -0500

    praying

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./Portfolio.css";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import projects from "../../projects.json";
-import trackImage from "../../assets/thumbnails/comicBookFinder.png";
-import seniorImage from "../../assets/thumbnails/football-futbol.png";
+import comicImage from "../../assets/thumbnails/comicBookFinder.png";
+import footballImage from "../../assets/thumbnails/football-futbol.png";
 import quizImage from "../../assets/thumbnails/JavascriptDOM.png";
-import passwordImage from "../../assets/thumbnails/workday.png";
+import workImage from "../../assets/thumbnails/workday.png";
 
 function Portfolio() {
   return (
@@ -18,52 +18,28 @@ function Portfolio() {
       <div className="row">
         <div className="col-sm-6">
           <ProjectCard
-            id={projects[6].id}
-            key={projects[6].id}
-            name={projects[6].name}
-            image={projects[6].image}
-            alt={projects[6].alt}
-            description={projects[6].description}
-            link={projects[6].link}
-          />
-        </div>
-        <div className="col-sm-6">
-          <ProjectCard
-            id={projects[7].id}
-            key={projects[7].id}
-            name={projects[7].name}
-            image={projects[7].image}
-            alt={projects[7].alt}
-            description={projects[7].description}
-            link={projects[7].link}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-6">
-          <ProjectCard
             id={projects[0].id}
             key={projects[0].id}
             name={projects[0].name}
-            image={trackImage}
+            image={projects[0].image}
             alt={projects[0].alt}
             description={projects[0].description}
             link={projects[0].link}
           />
         </div>
+      </div>
+      <div className="row">
         <div className="col-sm-6">
           <ProjectCard
             id={projects[1].id}
             key={projects[1].id}
             name={projects[1].name}
-            image={seniorImage}
+            image={projects[1].image}
             alt={projects[1].alt}
             description={projects[1].description}
             link={projects[1].link}
           />
         </div>
-      </div>
-      <div className="row">
         <div className="col-sm-6">
           <ProjectCard
             id={projects[2].id}
@@ -75,30 +51,32 @@ function Portfolio() {
             link={projects[2].link}
           />
         </div>
+      </div>
+      <div className="row">
         <div className="col-sm-6">
           <ProjectCard
             id={projects[3].id}
             key={projects[3].id}
             name={projects[3].name}
-            image={quizImage}
+            image={projects[3].image}
             alt={projects[3].alt}
             description={projects[3].description}
             link={projects[3].link}
           />
         </div>
-      </div>
-      <div className="row">
         <div className="col-sm-6">
           <ProjectCard
             id={projects[4].id}
             key={projects[4].id}
             name={projects[4].name}
-            image={passwordImage}
+            image={projects[4].image}
             alt={projects[4].alt}
             description={projects[4].description}
             link={projects[4].link}
           />
         </div>
+      </div>
+      <div className="row">
         <div className="col-sm-6">
           <ProjectCard
             id={projects[5].id}
@@ -110,6 +88,17 @@ function Portfolio() {
             link={projects[5].link}
           />
         </div>
+        {/* <div className="col-sm-6">
+          <ProjectCard
+            id={projects[0].id}
+            key={projects[0].id}
+            name={projects[0].name}
+            image={projects[0].image}
+            alt={projects[0].alt}
+            description={projects[0].description}
+            link={projects[0].link}
+          />
+        </div> */}
       </div>
     </div>
   );
