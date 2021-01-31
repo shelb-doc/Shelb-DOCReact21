a28e63d200837d58b9dcc79f521276d9b82fb957
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-01-30 21:01:21 -0500

    fixed react bug

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,11 +22,11 @@ return (
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
+        {/* <li className="nav-item">
             <NavLink to="/about" className="nav-link" activeClassName="active">
             About
             </NavLink>
-        </li>
+        </li> */}
         <li className="nav-item">
             <NavLink
             to="/contact"
